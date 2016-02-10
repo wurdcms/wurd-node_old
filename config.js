ffac839831577073f6ea1acdf4497d372157e05e
Charles Davison <charlie@powmedia.co.uk>
2016-02-10 18:25:01 +0000

    Use api.wurd.io

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   api: {
-    url: process.env.WURD_API_URL || 'https://api-v2.wurd.io/v1'
+    url: process.env.WURD_API_URL || 'https://api.wurd.io/v1'
   },
 
   maxAgeMs: 60*1000 //1 minute
